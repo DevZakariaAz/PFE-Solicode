03924744b02de29d961783a2f37ab9982a6cc983
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2024-05-23 10:20:36 +0100

    Area

---
+++ Codage/HomePage.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Explore the beauty of Tangier</title>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="./style/NavBar.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap">
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg ">
+    <a class="navbar-brand" href="#">
+      <img src="./img/LogoTnagorroco.png" width="180" height="auto" class="d-inline-block align-top" alt="Logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">about</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Restaurants</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">destination</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <hr>
+
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-6 text-center text-md-left">
+        <h1 class="display-4">Explore the beauty of Tangier</h1>
+        <form class="form-inline justify-content-center justify-content-md-start mt-3">
+          <input class="form-control mr-2" type="search" placeholder="search destinations" aria-label="Search">
+          <button class="btn btn-primary" type="submit">Explore</button>
+        </form>
+        <p class="mt-3">
+          Still deciding where to go? <a href="#">Explore destinations</a>
+        </p>
+      </div>
+      <div class="col-md-6 text-center">
+        <img src="./img/Miramonte.pnj.jpg" class="img-fluid rounded" alt="Tangier">
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap JS, Popper.js, and jQuery -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>

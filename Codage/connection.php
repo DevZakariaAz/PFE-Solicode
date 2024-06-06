9d0b23f96a6a6bad8dfbfb403ef4a1aa8221674a
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2024-06-06 16:34:02 +0100

    modified in file connection and add some fonctions

--- Codage/connection.php
+++ Codage/connection.php
@@ -9,4 +9,57 @@ try {
 } catch (PDOException $e) {
     echo 'failed ' . $e->getMessage();
 }
+function fetchTopRestaurants($pdo) {
+    $query = "
+    SELECT 
+        p.publicationid, 
+        p.titre, 
+        p.description, 
+        p.coverimage, 
+        AVG(r.rating) as average_rating
+    FROM 
+        publication p
+    LEFT JOIN 
+        review r ON p.publicationid = r.publicationid
+    WHERE 
+        p.location = 'Tangier' AND p.type = 'restaurant'
+    GROUP BY 
+        p.publicationid
+    ORDER BY 
+        average_rating DESC
+    LIMIT 3";
+
+    $statement = $pdo->prepare($query);
+    $statement->execute();
+
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
+}
+function fetchTopDestinations($DB) {
+    $query = "
+    SELECT 
+    p.publicationid, 
+    p.titre, 
+    p.description, 
+    p.coverimage, 
+    COUNT(r.reviewid) as review_count,
+    AVG(r.rating) as average_rating
+FROM 
+    publication p
+LEFT JOIN 
+    review r ON p.publicationid = r.publicationid
+WHERE 
+    p.location = 'Tangier'
+GROUP BY 
+    p.publicationid
+ORDER BY 
+    average_rating DESC
+LIMIT 3;
+";
+
+    $statement = $DB->prepare($query);
+    $statement->execute();
+
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
+}
+
 ?>
